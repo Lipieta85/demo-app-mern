f0c383e72c63aba539de1cc7774ac57cc1b7e372
Rafał Lipieta <cwgflojdid@wp.pl>
2018-09-11 09:19:39 +0200

    Update PostActions.js

--- client/modules/Post/PostActions.js
+++ client/modules/Post/PostActions.js
@@ -5,8 +5,6 @@ export const ADD_POST = 'ADD_POST';
 export const ADD_POSTS = 'ADD_POSTS';
 export const EDIT_POST = 'EDIT_POST';
 export const DELETE_POST = 'DELETE_POST';
-export const THUMB_UP = 'THUMB_UP';
-export const THUMB_DOWN = 'THUMB_DOWN';
 
 // Export Actions
 export function addPost(post) {
@@ -83,28 +81,6 @@ export function deletePostRequest(cuid) {
   };
 }
 
-export function updateVotes(cuid, post) {
-  return {
-    type: UPDATE_VOTES,
-    cuid,
-    post
-  };
-}
-
-export function thumbUp(cuid) {
-  return {
-    type: THUMB_UP,
-    cuid,
-  };
-}
-
-export function thumbDown(cuid) {
-  return {
-    type: THUMB_DOWN,
-    cuid,
-  };
-}
-
 export function thumbUpRequest(cuid) {
   return dispatch => {
     return callApi(`posts/${cuid}/up`, 'put').then((post) => dispatch(editPost(cuid, post)));
@@ -113,7 +89,7 @@ export function thumbUpRequest(cuid) {
 
 export function thumbDownRequest(cuid) {
   return dispatch => {
-    return callApi(`posts/${cuid}/down`, 'put').then((post) => dispatch(ediPost(cuid, post)));
+    return callApi(`posts/${cuid}/down`, 'put').then((post) => dispatch(editPost(cuid, post)));
   };
 }
 
